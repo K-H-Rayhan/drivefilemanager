36e27bebb34275acbb9009a5d4e41904d33ec632
Kamrul Hasan Rayhan <aahim7406@gmail.com>
2023-04-01 09:57:05 +0600

    test

--- hooks/useHandleTree.tsx
+++ hooks/useHandleTree.tsx
@@ -1,41 +1,41 @@
-import { useState, useReducer } from "react";
-import useLocalStorage, { FolderTree } from "@/hooks/useLocalStorage";
+// import { useState, useReducer } from "react";
+// import useLocalStorage, { FolderTree } from "@/hooks/useLocalStorage";
 
 
 
-const reducer = (state, action) => {
-    switch (action.type) {
-        case "COMPLETE":
-            return state.map((todo) => {
-                if (todo.id === action.id) {
-                    return { ...todo, complete: !todo.complete };
-                } else {
-                    return todo;
-                }
-            });
-        default:
-            return state;
-    }
-};
+// const reducer = (state, action) => {
+//     switch (action.type) {
+//         case "COMPLETE":
+//             return state.map((todo) => {
+//                 if (todo.id === action.id) {
+//                     return { ...todo, complete: !todo.complete };
+//                 } else {
+//                     return todo;
+//                 }
+//             });
+//         default:
+//             return state;
+//     }
+// };
 
-const useHandleTree = (): [FolderTree[]] => {
-    const [storedValue, setStoredValue] = useLocalStorage("tree")
-    const [storedFolderData, dispatch] = useReducer<FolderTree[]>(reducer, storedValue);
+// const useHandleTree = (): [FolderTree[]] => {
+//     const [storedValue, setStoredValue] = useLocalStorage("tree")
+//     const [storedFolderData, dispatch] = useReducer<FolderTree[]>(reducer, storedValue);
 
 
-    const handleAddFolder = (folder: FolderTree) => {
-    };
+//     const handleAddFolder = (folder: FolderTree) => {
+//     };
 
-    const handleAddFile = (name) => {
+//     const handleAddFile = (name) => {
         
-    };
-    const handleDeleteFolder = (folder: FolderTree) => {
-    };
-    const handleDeleteFile = (file: File) => {
-    };
+//     };
+//     const handleDeleteFolder = (folder: FolderTree) => {
+//     };
+//     const handleDeleteFile = (file: File) => {
+//     };
 
 
-    return [storedFolderData];
-};
+//     return [storedFolderData];
+// };
 
-export default useHandleTree;
+// export default useHandleTree;
