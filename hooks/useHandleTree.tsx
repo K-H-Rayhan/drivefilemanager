daaae608537597c7204eca60cb5f69f6651f1c77
Kamrul Hasan Rayhan <aahim7406@gmail.com>
2023-04-01 16:47:39 +0600

    delete done

--- hooks/useHandleTree.tsx
+++ hooks/useHandleTree.tsx
@@ -4,7 +4,17 @@ import useLocalStorage, { FolderTree } from "@/hooks/useLocalStorage";
 type HandleTree = {
     storedFolderData: FolderTree[],
     handleAddFolder: (name: string, parentId: string) => void,
+    handleEditFolder: (name: string, id: string) => void,
+    handleDeleteFolder: (id: string) => void,
+    handleAddFile: (name: string, parentId: string) => void,
+    handleEditFile: (name: string, id: string, parentId: string) => void,
+    handleDeleteFile: (id: string, parentId: string) => void,
 }
+
+type INITACTION = {
+    type: "INIT";
+    payload: FolderTree[];
+};
 
 type ADDFOLDERACTION = {
     type: "ADD_FOLDER";
@@ -13,12 +23,52 @@ type ADDFOLDERACTION = {
         parentId: string,
     };
 };
-type INITACTION = {
-    type: "INIT";
-    payload: FolderTree[];
+
+type DELETEFOLDER = {
+    type: "DELETE_FOLDER";
+    payload: {
+        id: string,
+    };
 };
 
-type HandleTreeAction = ADDFOLDERACTION | INITACTION
+type EDITFOLDER = {
+    type: "EDIT_FOLDER";
+    payload: {
+        id: string,
+        name: string,
+    };
+};
+
+type ADDFILEACTION = {
+    type: "ADD_FILE";
+    payload: {
+        name: string,
+        parentId: string,
+    };
+};
+
+type EDITFILE = {
+    type: "EDIT_FILE";
+    payload: {
+        id: string,
+        name: string,
+        parentId: string,
+    };
+};
+
+type DELETEFILE = {
+    type: "DELETE_FILE";
+    payload: {
+        id: string,
+        parentId: string,
+    };
+};
+
+type FOLDEROPTIONS = ADDFOLDERACTION | DELETEFOLDER | EDITFOLDER
+
+type FILEOPTIONS = ADDFILEACTION | DELETEFILE | EDITFILE
+
+type HandleTreeAction = INITACTION | FOLDEROPTIONS | FILEOPTIONS
 
 type AddNode = {
     name: string,
@@ -27,10 +77,11 @@ type AddNode = {
 
 const generateSlug = (data: string) => data.replace(/\s/g, "-").toLowerCase()
 
+
+// If parentId found and slug doesn't exist on that child add item
 const addNode = (data: AddNode, initData: FolderTree[]): FolderTree[] => {
     const newData = [...initData]
     for (let i = 0; i < newData.length; i++) {
-        // If parentId found and slug doesn't exist on that child add item
         const slug = generateSlug(data.name)
         if (newData[i].id == data.parentId && !newData[i].children.some(item => item.slug == slug)) {
             newData[i].children.push({
@@ -48,6 +99,21 @@ const addNode = (data: AddNode, initData: FolderTree[]): FolderTree[] => {
     return newData
 }
 
+// If delete node id found filter the item and return new array
+const deleteNode = (id: string, initData: FolderTree[]): FolderTree[] => {
+    const newData = [...initData]
+    for (let i = 0; i < newData.length; i++) {
+        if (newData[i].children.some(item => item.id == id)) {
+            newData[i].children = newData[i].children.filter(item => item.id != id)
+            return newData
+        }
+        if (newData[i].children.length > 0) {
+            deleteNode(id, newData[i].children)
+        }
+    }
+    return newData
+}
+
 const reducer = (storedFolderData: FolderTree[], action: HandleTreeAction): FolderTree[] => {
     switch (action.type) {
         case "INIT":
@@ -57,6 +123,8 @@ const reducer = (storedFolderData: FolderTree[], action: HandleTreeAction): Fold
                 name: action.payload.name,
                 parentId: action.payload.parentId,
             }, storedFolderData)
+        case "DELETE_FOLDER":
+            return deleteNode(action.payload.id, storedFolderData)
         default:
             return storedFolderData;
     }
@@ -93,30 +161,73 @@ const useHandleTree = (): HandleTree => {
 
     // Edit a folder from the tree
     const handleEditFolder = (name: string, id: string) => {
+        dispatch({
+            type: "EDIT_FOLDER",
+            payload:
+            {
+                id: id,
+                name: name,
+            }
+        });
     };
 
     // Delete a folder from the tree
     const handleDeleteFolder = (id: string) => {
+        dispatch({
+            type: "DELETE_FOLDER",
+            payload:
+            {
+                id: id
+            }
+        });
     };
 
     // Add a new file to the tree
-    const handleAddFile = (name: string) => {
-        let id = self.crypto.randomUUID();
+    const handleAddFile = (name: string, parentId: string) => {
+        dispatch({
+            type: "ADD_FILE",
+            payload:
+            {
+                name: name,
+                parentId: parentId,
+            }
+        });
     };
     // Edit a new file to the tree, parent id to make it efficient
-    const handleEditFile = (name: string, parentId: string) => {
+    const handleEditFile = (id: string, name: string, parentId: string) => {
+        dispatch({
+            type: "EDIT_FILE",
+            payload:
+            {
+                id: id,
+                name: name,
+                parentId: parentId,
+            }
+        });
     };
 
 
     // Delete a file from the tree, parent id to make it efficient
-    const handleDeleteFile = (id: File, parentId: string) => {
-
+    const handleDeleteFile = (id: string, parentId: string) => {
+        dispatch({
+            type: "DELETE_FILE",
+            payload:
+            {
+                id: id,
+                parentId: parentId,
+            }
+        });
     };
 
 
     return {
         storedFolderData,
         handleAddFolder,
+        handleEditFolder,
+        handleDeleteFolder,
+        handleAddFile,
+        handleEditFile,
+        handleDeleteFile,
     };
 };
 
