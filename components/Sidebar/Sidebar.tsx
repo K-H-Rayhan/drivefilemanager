2632cd01e2eb563b47c4fa3765b6be2acc1a8741
Kamrul Hasan Rayhan <aahim7406@gmail.com>
2023-04-04 13:33:51 +0600

    fixed width

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -17,7 +17,7 @@ function Sidebar({ }: Props) {
 
     return (
         <div style={{
-            maxWidth: '257px',
+            width: '257px',
             height: '100vh',
             paddingInline: '10px',
             color: '#000',
